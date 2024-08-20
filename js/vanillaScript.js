7c484e8c9f630b7f110c508368f5f6238e4aec1d
UIUX웹퍼블리싱 <50073413+pam7461@users.noreply.github.com>
2024-08-20 15:05:21 +0900

    Add files via upload

--- js/vanillaScript.js
+++ js/vanillaScript.js
@@ -13,36 +13,38 @@ window.onload=function(){
                     $('.curation-poster p').html(`MEGABOX<br>FILM<br>SOCIETY`);
                 }else{
                     $('.curation-poster p').html(`MEGABOX<br>CLASSIC<br>SOCIETY`);
-                }
+                };
                 $('.curation-poster img').attr('src',`./images/${data.src}`);                        
             },
             error:function(){
                 console.log('Error');
             },
         });
-    }
+    };
+
+    
     fnCuration(1);
     // 보이기 함수           
     let fnShow = (element,displayValue)=>{
         element.style.display = displayValue;
-    }
+    };
     // 감추기 함수
     let fnHide = (element)=>{
         element.style.display = 'none';
-    }
+    };
     // 난수 발생 함수
     let fnRandom = (divNum)=>{
         let idx = Math.floor(Math.random() * 100 % divNum);
         return idx;
-    }
+    };
     // 클래스 추가
     let fnAddClass = (element,className)=>{
         element.classList.add(className);                
-    }
+    };
     // 클래스 제거
     let fnRemoveClass = (element,className)=>{
         element.classList.remove(className);
-    }
+    };
     // BOX OFFICE SWIPER JS
     let boxOfficeSwiper = new Swiper('.box-office-swiper',{
         slidesPerView:2.4,
@@ -130,8 +132,8 @@ window.onload=function(){
             benefitSwiper01.autoplay.stop();
         }else{
             benefitSwiper01.autoplay.start();
-        }
-    }
+        };
+    };
 
 
     // 문서가 시작할대 쿠키에 따른 팝업 이벤트            
@@ -139,7 +141,7 @@ window.onload=function(){
         fnHide(popup);
     }else{
         fnShow(popup,'block');
-    }
+    };
 
     // 랜덤 광고            
     fnShow(NodeList01[fnRandom(4)],'block');
@@ -153,13 +155,13 @@ window.onload=function(){
                 
     ad01Close.onclick = ()=>{                           
         fnHide(document.querySelector('.ad01'));
-    }
+    };
 
     // 링크에 타이틀 넣기
     
     for(let idx=0;idx<=allA.length-1;idx++){
         allA[idx].title = `${allA[idx].innerText} 가기`;
-    }
+    };
     
     // 서브네비게이션 호출
     
@@ -169,16 +171,16 @@ window.onload=function(){
             fnAddClass(header,'active');
             for(let idx=0;idx<=mainNav.length-1;idx++){
                 fnRemoveClass(mainNav[idx],'active');
-            }
+            };
             fnAddClass(e.target,'active');
-        }
-    }
+        };
+    };
     
     // 서브네비게이션 닫기            
     header.onmouseleave = (e)=>{
         for(let idx2=0;idx2<=mainNav.length-1;idx2++){
             fnRemoveClass(mainNav[idx2],'active');
-        }
+        };
         fnRemoveClass(e.target,'active');
     };
 
@@ -188,13 +190,13 @@ window.onload=function(){
         let loginWindow = document.querySelector('.login');
         fnAddClass(loginWindow,'active');                
         e.preventDefault();
-    }
+    };
     // 로그인 닫기
     
     btnClose.onclick = ()=>{
         let loginWindow = document.querySelector('.login');
         fnRemoveClass(loginWindow,'active');
-    }
+    };
 
     // popup swiper JS
     let popupSwiper = new Swiper('.swiper-container',{
@@ -214,10 +216,10 @@ window.onload=function(){
         let chkNoshow = document.querySelector('#chk-noshow');               
         if(chkNoshow.checked){
             document.cookie = `TMP=true;path=/;max-age=${24*60*60}`;
-        }
+        };
         let popup = document.querySelector('.popup');
         fnHide(popup);
-    }
+    };
 
      // popup 슬라이더 멈춤
     
@@ -225,13 +227,13 @@ window.onload=function(){
         fnRemoveClass(e.target,'active');                
         fnAddClass(btnStart,'active');
         popupSwiper.autoplay.stop();
-    }
+    };
     // popup 슬라이더 재생
     btnStart.onclick = (e)=>{
         fnRemoveClass(e.target,'active');                
         fnAddClass(btnStop,'active');
         popupSwiper.autoplay.start();
-    }
+    };
     
     // QNAV
     
@@ -245,14 +247,14 @@ window.onload=function(){
                         fnShow(headerArt[idx],'block');
                     }else{
                         fnHide(headerArt[idx]);
-                    }
+                    };
                 }else{
                     fnHide(headerArt[idx]);
-                }
-            }
+                };
+            };
             e.preventDefault();
-        }
-    }
+        };
+    };
   
 
     //SEARCH TAB
@@ -262,20 +264,20 @@ window.onload=function(){
             e.preventDefault();
             for(let idx=0;idx<=searchTit.length-1;idx++){
                 fnRemoveClass(searchTit[idx],'active');
-            }
+            };
             fnAddClass(e.target,'active');
             let tab = document.querySelectorAll('.tab');
             for(let idx=0;idx<=searchTit.length-1;idx++){
                 fnRemoveClass(tab[idx],'active');
-            }
+            };
             let thisText = e.target.innerText;
             if(thisText == '예매율 순'){
                 fnAddClass(tab[0],'active');
             }else{
                 fnAddClass(tab[1],'active');
-            }
-        }
-    }
+            };
+        };
+    };
     
 
     // SEARCH MOVIE POSTER
@@ -286,10 +288,10 @@ window.onload=function(){
             let acMoviePoster = document.querySelectorAll('.tab.active .movie-poster li');
             for(let idx=0;idx<=acMoviePoster.length-1;idx++){
                 fnRemoveClass(acMoviePoster[idx],'active');
-            }
+            };
             fnAddClass(acMoviePoster[acIdx],'active');
-        }
-    }
+        };
+    };
     
 
     // 모바일 이벤트
@@ -299,8 +301,8 @@ window.onload=function(){
         let confirmMsg = confirm('로그인 후 사용가능합니다.\n로그인 하시겠습니까?');
         if(confirmMsg){
             fnAddClass(document.querySelector('.login'),'active');
-        }
-    }
+        };
+    };
     
 
 
@@ -308,24 +310,24 @@ window.onload=function(){
     
     mBtnNav.onclick = ()=>{
         fnShow(document.querySelector('.m-nav'),'block');
-    }           
+    };          
 
     // M-NAV 닫기
     
     mBtnNavClose.onclick = ()=>{
         fnHide(document.querySelector('.m-nav'));
-    }
+    };
     // LOGIN SHOW    
     mLoginShow.onclick = (e)=>{
         e.preventDefault();
         fnHide(document.querySelector('.m-nav'));
         fnAddClass(document.querySelector('.login'),'active');
-    } 
+    }; 
     // CURATION LIST     
     for(let idx=0;idx<=curationList.length-1;idx++){
         curationList[idx].onclick = function(e){
             e.preventDefault();
             fnCuration(idx + 1);
-        }
-    }
-}
+        };
+    };
+};
